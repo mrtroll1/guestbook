d6fa7540897f43d466237c973c344e1223a6931c
luka <luka.asfari@gmail.com>
2025-05-15 14:27:30 +0200

    comment cleaup command

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -43,7 +45,7 @@ class ConferenceController extends AbstractController
         return $response;
     }
 
-    #[Route('/conference-header', name: 'conference-header')]
+    #[Route('/conference-header', name: 'conference_header')]
     public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
         $response = new Response($this->twig->render('conference/header.html.twig', [
@@ -55,7 +57,12 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response {
+    public function show(Request $request, 
+        Conference $conference, 
+        CommentRepository $commentRepository,
+        NotifierInterface $notifier, 
+        string $photoDir
+    ): Response {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
@@ -83,7 +90,14 @@ class ConferenceController extends AbstractController
 
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted())
+        {
+            $notifier->send(new Notification('Please check your submission. There are some issues.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
@@ -15,10 +16,35 @@ class CommentRepository extends ServiceEntityRepository
 {
     public const PAGINATOR_PER_PAGE = 2;
 
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
     }
+
+    public function countOldRejected(): int 
+    {
+        return $this->getOldRejectedQueryBuilder()->select('count(c.id')->getQuery()->getSingleScalarResult();
+    }
+
+    public function deleteOldRejected(): int 
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        $threshold = new \DateTimeImmutable(sprintf('-%d days', self::DAYS_BEFORE_REJECTED_REMOVAL));
+    
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :state_rejected OR c.state = :state_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameter('state_rejected', 'rejected')
+            ->setParameter('state_spam', 'spam')
+            ->setParameter('date', $threshold)
+        ;
+    }    
 
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
     {
